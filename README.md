54dd4d7cf9b01cc882c53034efc6c7bda131b77d
Osvaldas Valutis <osvaldas.v@gmail.com>
2018-02-06 17:29:21 +0200

    First version

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# sass-utils
-Kollegorna's SASS utilities
+# Kollegorna's SASS utilities
+
+A starter package for every SASS project.
+
+## Usage
+
+1. Install with `npm install --save`
+2. Include `src/_all.scss` in your project. What it does:
+  - Makes all of the SASS utilities available
+  - Injects Reset CSS
+
+## Development
+
+Run `gulp` when developing. This will:
+
+- Run the linter for your own good
+- Generate `reset.css` file for non-SASS projects
+
+## Else...
+
+For now, browse `src/*` to get to know the utilities. Documentation are on the way...
---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const stylelint = require('gulp-stylelint')
+
+gulp.task('sass', () => {
+  return gulp.src('./src/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('stylelint', () => {
+  return gulp.src('./src/**/*.scss')
+  .pipe(stylelint({
+    reporters: [
+      {formatter: 'string', console: true}
+    ]
+  }))
+})
+
+gulp.task('default', ['stylelint', 'sass'], () => {
+  gulp.watch('./src/**/*.scss', ['stylelint', 'sass'])
+})
